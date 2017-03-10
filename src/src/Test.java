000179a5a3297f093f0189ec0e422e0ec0806f55
Charlotte Clapp <cac23@st-andrews.ac.uk>
2017-03-10 02:26:30 +0000

    added in ioimages

--- src/src/Test.java
+++ src/src/Test.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import javax.imageio.ImageIO;
 
 final public class Test {
     static JFrame frame;
@@ -43,12 +44,30 @@ final public class Test {
         ServerSocket listener;
         Socket socket;
         BufferedReader reader;
-        int[] leftTurnTime = new int[]{3,5,7,9,10,20};
+        int[] leftTurnTime = new int[]{3,5};
+        int[] rightTurnTime = new int[]{3,6};
         boolean speedFirstReceived = false;
         long startTime;
 
+        Image turnLeftImage, speedLimitImageOne, speedLimitImageTwo, trafficAheadImage,
+                lowTirePressureImage, compassImage, streetNameImage;
+
+        String baseDirectory = "images/";
+
+
+
         public DrawPanel() {
             try {
+                //added in
+                turnLeftImage = ImageIO.read(getClass().getResource(baseDirectory + "turnRight.png"));
+                speedLimitImageOne = ImageIO.read(getClass().getResource(baseDirectory + "firstSpeedLimitSign.png"));
+                speedLimitImageTwo = ImageIO.read(getClass().getResource(baseDirectory + "secondSpeedLimitSign.png"));
+                trafficAheadImage = ImageIO.read(getClass().getResource(baseDirectory + "trafficAhead.png"));
+                lowTirePressureImage = ImageIO.read(getClass().getResource(baseDirectory + "lowTirePressure.png"));
+                compassImage = ImageIO.read(getClass().getResource(baseDirectory + "directionAndCompass.png"));
+                streetNameImage = ImageIO.read(getClass().getResource(baseDirectory + "currentStreetName.png"));
+
+
                 listener = new ServerSocket(3001);
                 socket = listener.accept();
                 reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -70,6 +89,13 @@ final public class Test {
                 if (elapsedTime > time1 && (elapsedTime<time1 +1000)) {
                     g.setColor(Color.WHITE);
                     g.drawString("output", 100, 150);
+                }
+            }
+
+            for(int i =0; i < rightTurnTime.length; i++) {
+                long time1 = rightTurnTime[i]*1000;
+                if (elapsedTime > time1 && (elapsedTime<time1 +1000)) {
+                    //g.drawImage(turnRighttImage, 400, 400, turnRightImage.getWidth(null), turnRightImage.getHeight(null), null);
                 }
             }
 
