701ffe7239a53bc46164047593c808c18f48566c
Charlotte Clapp <cac23@st-andrews.ac.uk>
2017-03-11 22:54:24 +0000

    adjusted brackets and arrays/timings

--- src/src/Test.java
+++ src/src/Test.java
@@ -14,6 +14,7 @@ final public class Test {
     static DrawPanel drawPanel;
 
     public static void main(String... args) {
+        System.out.println("Game started:" + System.currentTimeMillis());
         Test.go();
     }
 
@@ -41,10 +42,14 @@ final public class Test {
         ServerSocket listener;
         Socket socket;
         BufferedReader reader;
-        int[] leftTurnTime = new int[]{2,6};
-        int[] rightTurnTime = new int[]{4,10};
+        int[] leftTurnTime = new int[]{3};
+        int[] currentStreetTime = new int[]{3,12};
+        int[] currentStreetTimeDuration = new int[]{3,5};
+        int[] lowTireTime = new int[]{21, 27};
+        int[] directionTime = new int[]{30,37};
+        int[] speedLimitTime = new int[]{40};
         boolean speedFirstReceived = false;
-        long startTime;
+        long startTime = System.currentTimeMillis();
 
         Image turnLeftImage, speedLimitImageOne, speedLimitImageTwo, lowTirePressureImage, compassImage;
 
@@ -58,12 +63,12 @@ final public class Test {
                 System.out.println("Client Connected!");
                 //added in
                     turnLeftImage = ImageIO.read(getClass().getResource("turnRight.png"));
-//                    speedLimitImageOne = ImageIO.read(getClass().getResource("firstSpeedLimitSign.png"));
-//                    speedLimitImageTwo = ImageIO.read(getClass().getResource("secondSpeedLimitSign.png"));
+                    speedLimitImageOne = ImageIO.read(getClass().getResource("firstSpeedLimitSign.png"));
+                    speedLimitImageTwo = ImageIO.read(getClass().getResource("secondSpeedLimitSign.png"));
 //                    //trafficAheadImage = ImageIO.read(getClass().getResource("trafficAhead.png"));
 //                    //trafficAheadImage = ImageIO.read(getClass().getResource(baseDirectory + "lowTirePressure.png"));
-                    //lowTirePressureImage = ImageIO.read(getClass().getResource("lowTirePressure.png"));
-//                    compassImage = ImageIO.read(getClass().getResource("directionAndCompass.png"));
+                    lowTirePressureImage = ImageIO.read(getClass().getResource("lowTirePressure.png"));
+                    compassImage = ImageIO.read(getClass().getResource("directionAndCompass.png"));
 
 
             } catch (IOException e) {
@@ -71,33 +76,60 @@ final public class Test {
             }
         }
         public void paintComponent(Graphics g) {
-            //DrawPanel drawPanel = new DrawPanel();
+            System.out.println("paintcomponent");
             long currentTime = System.currentTimeMillis();
             long elapsedTime = currentTime - startTime;
 
-            /* left turn signal */
-            for(int i =0; i < leftTurnTime.length; i++) {
-                long time1 = leftTurnTime[i]*1000;
-                if (elapsedTime > time1 && (elapsedTime<time1 +1000)) {
-                    g.drawImage(turnLeftImage, 400, 400, turnLeftImage.getWidth(null), turnLeftImage.getHeight(null), null);
-                }
-            }
-
-            /* current street */
-            for(int i =0; i < rightTurnTime.length; i++) {
-                long time1 = rightTurnTime[i]*1000;
-                if (elapsedTime > time1 && (elapsedTime<time1 +1000)) {
-                        g.setColor(Color.GREEN);
-                        g.setFont(new Font("TimesRoman", Font.PLAIN, 80));
-                        g.drawString("Market St", 500, 800);
-                }
-            }
-
-//            /* low tire pressure */
+//            /* left turn signal */
 //            for(int i =0; i < leftTurnTime.length; i++) {
 //                long time1 = leftTurnTime[i]*1000;
-//                if (elapsedTime > time1 && (elapsedTime<time1 +1000)) {
-//                    g.drawImage(lowTirePressureImage, 400, 400, lowTirePressureImage.getWidth(null), lowTirePressureImage.getHeight(null), null);
+//                if (elapsedTime > time1 && (elapsedTime<time1 +4000)) {
+//                    System.out.println("Here");
+//                    g.drawImage(turnLeftImage, 900, 1100, turnLeftImage.getWidth(null), turnLeftImage.getHeight(null), null);
+//                }
+//            }
+
+//             /* first speed limit */
+//            for(int i =0; i < speedLimitTime.length; i++) {
+//                System.out.println("i:"+i);
+//                long time2 = speedLimitTime[i]*1000;
+//                if (elapsedTime > time2 && (elapsedTime<time2 +1000)) {
+//                    System.out.println("elapsed time");
+//                    g.drawImage(speedLimitImageOne, 1000, 1100, speedLimitImageOne.getWidth(null), speedLimitImageOne.getHeight(null), null);
+//                }
+//            }
+
+//             /* second speed limit */
+//            for(int i =0; i < speedLimitTime.length; i++) {
+//                long time2 = speedLimitTime[i]*1000;
+//                if (elapsedTime > time2 && (elapsedTime<time2 +1000)) {
+//                    g.drawImage(speedLimitImageTwo, 1000, 1100, speedLimitImageTwo.getWidth(null), speedLimitImageTwo.getHeight(null), null);
+//                }
+//            }
+//
+            /* current street */
+            for(int i = 0; i < currentStreetTime.length; i++) {
+                long time3 = currentStreetTime[i]*1000;
+                if (elapsedTime > time3 && (elapsedTime<(time3 +1000*currentStreetTimeDuration[i]))) {
+                        g.setColor(Color.WHITE);
+                        g.setFont(new Font("TimesRoman", Font.BOLD, 100));
+                        g.drawString("Market St", 1100, 1200);
+                }
+            }
+//
+//            /* low tire pressure */
+//            for(int i =0; i < lowTireTime.length; i++) {
+//                long time4 = lowTireTime[i]*1000;
+//                if (elapsedTime > time4 && (elapsedTime<time4 +1000)) {
+//                    g.drawImage(lowTirePressureImage, 1100, 1200, lowTirePressureImage.getWidth(null), lowTirePressureImage.getHeight(null), null);
+//                }
+//            }
+//
+//            /* direction */
+//            for(int i =0; i < directionTime.length; i++) {
+//                long time5 = directionTime[i]*1000;
+//                if (elapsedTime > time5 && (elapsedTime<time5 +1000)) {
+//                    g.drawImage(compassImage, 1000, 1100, compassImage.getWidth(null), compassImage.getHeight(null), null);
 //                }
 //            }
 
@@ -111,8 +143,8 @@ final public class Test {
                             this.startTime = System.currentTimeMillis();
                         }
                         g.setColor(Color.WHITE);
-                        g.setFont(new Font("TimesRoman", Font.PLAIN, 80));
-                        g.drawString("Speed: " +line, 700, 1200);
+                        g.setFont(new Font("TimesRoman", Font.BOLD, 150));
+                        g.drawString(line, 1280, 1400);
                     }
                 }
             } catch (IOException e) {
